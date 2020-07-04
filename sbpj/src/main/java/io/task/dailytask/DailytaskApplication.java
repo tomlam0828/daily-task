6955e35853f01ef0acf4e62d00a8238ff5cfe4ad
Tom Lam <tomlam0828@gmail.com>
2020-07-04 00:29:40 -0700

    first

---
+++ sbpj/src/main/java/io/task/dailytask/DailytaskApplication.java
@@ -0,0 +1,13 @@
+package io.task.dailytask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailytaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DailytaskApplication.class, args);
+    }
+
+}
