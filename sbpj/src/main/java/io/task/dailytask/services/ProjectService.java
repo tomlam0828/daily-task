90e2895e7b962729ac4fe1b6298dee32ceb5a5ca
Tom Lam <tomlam0828@gmail.com>
2020-07-04 14:09:27 -0700

    second update

---
+++ sbpj/src/main/java/io/task/dailytask/services/ProjectService.java
@@ -0,0 +1,22 @@
+package io.task.dailytask.services;
+
+import io.task.dailytask.domain.Project;
+import io.task.dailytask.exceptions.ProjectIdException;
+import io.task.dailytask.repositories.ProjectRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProjectService {
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    public Project saveOrUpdateProject(Project project) {
+        try {
+            project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
+        }
+    }
+}
