bb1aa643ee2494fa20be01712afa5bed220f8910
Tom Lam <tomlam0828@gmail.com>
2020-07-04 17:18:09 -0700

    react-update

--- daily-task-client/src/components/Dashboard.js
+++ daily-task-client/src/components/Dashboard.js
@@ -1,9 +1,15 @@
 import React, { Component } from "react";
+import ProjectItem from "./Project/ProjectItem";
 
 class Dashboard extends Component {
-  render() {
-    return <h1>Welcome to the dashboard</h1>;
-  }
+    render() {
+        return (
+            <div>
+                <h1>Welcome to the dashboard</h1>
+                <ProjectItem />
+            </div>
+        );
+    }
 }
 
 export default Dashboard;
