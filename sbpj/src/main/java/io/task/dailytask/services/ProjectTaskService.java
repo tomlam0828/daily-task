8a33db6a83819129b1ad127405dde1ac70fb389e
Tom Lam <tomlam0828@gmail.com>
2020-07-09 01:24:26 -0700

    backlog find by id

--- sbpj/src/main/java/io/task/dailytask/services/ProjectTaskService.java
+++ sbpj/src/main/java/io/task/dailytask/services/ProjectTaskService.java
@@ -68,4 +68,9 @@ public class ProjectTaskService {
 
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
+
+    public ProjectTask findPTByProjectSequence(String backlog_id, String pt_id) {
+
+        return projectTaskRepository.findByProjectSequence(pt_id);
+    }
 }
