bdceb8fade525200218f58ba3b8d168b537f0bda
Tom Lam <tomlam0828@gmail.com>
2020-07-08 17:51:04 -0700

    handling exception

--- sbpj/src/main/java/io/task/dailytask/exceptions/CustomResponseEntityExceptionHandler.java
+++ sbpj/src/main/java/io/task/dailytask/exceptions/CustomResponseEntityExceptionHandler.java
@@ -15,6 +15,12 @@ public class CustomResponseEntityExceptionHandler extends ResponseEntityExceptio
     @ExceptionHandler
     public final ResponseEntity<Object> handleProjectIdException(ProjectIdException ex, WebRequest request) {
         ProjectIdExceptionResponse exceptionResponse = new ProjectIdExceptionResponse(ex.getMessage());
-        return new ResponseEntity(exceptionResponse, HttpStatus.CREATED);
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler
+    public final ResponseEntity<Object> handleProjectNotFoundException(ProjectNotFoundException ex, WebRequest request) {
+        ProjectNotFoundExceptionResponse exceptionResponse = new ProjectNotFoundExceptionResponse(ex.getMessage());
+        return new ResponseEntity(exceptionResponse, HttpStatus.BAD_REQUEST);
     }
 }
--- sbpj/src/main/java/io/task/dailytask/services/ProjectTaskService.java
+++ sbpj/src/main/java/io/task/dailytask/services/ProjectTaskService.java
@@ -1,8 +1,11 @@
 package io.task.dailytask.services;
 
 import io.task.dailytask.domain.Backlog;
+import io.task.dailytask.domain.Project;
 import io.task.dailytask.domain.ProjectTask;
+import io.task.dailytask.exceptions.ProjectNotFoundException;
 import io.task.dailytask.repositories.BacklogRepository;
+import io.task.dailytask.repositories.ProjectRepository;
 import io.task.dailytask.repositories.ProjectTaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -17,39 +20,52 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
+    @Autowired
+    private ProjectRepository projectRepository;
+
     public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask) {
+        try {
+            // pts to added to a specific project, project != null
+            Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
 
-        // pts to added to a specific project, project != null
-        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+            // set the bl to the pt
+            projectTask.setBacklog(backlog);
 
-        // set the bl to the pt
-        projectTask.setBacklog(backlog);
+            // we want our project sequence to be like this: idpro-1, idpro-2
+            Integer BacklogSequence = backlog.getPTSequence();
 
-        // we want our project sequence to be like this: idpro-1, idpro-2
-        Integer BacklogSequence = backlog.getPTSequence();
+            // update the bl sequence
+            BacklogSequence++;
 
-        // update the bl sequence
-        BacklogSequence++;
+            backlog.setPTSequence(BacklogSequence);
 
-        backlog.setPTSequence(BacklogSequence);
+            // add sequence to project task
+            projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
+            projectTask.setProjectIdentifier(projectIdentifier);
 
-        // add sequence to project task
-        projectTask.setProjectSequence(projectIdentifier + "-" + BacklogSequence);
-        projectTask.setProjectIdentifier(projectIdentifier);
+            // initial priority when priority null
+            if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
+                projectTask.setPriority(3);
+            }
+            // initial status when status is null;
+            if (projectTask.getPriority() == null || projectTask.getStatus() == "") {
+                projectTask.setStatus("Todo");
+            }
 
-        // initial priority when priority null
-        if (projectTask.getPriority() == null || projectTask.getPriority() == 0) {
-            projectTask.setPriority(3);
+            return projectTaskRepository.save(projectTask);
+        } catch (Exception e) {
+            throw new ProjectNotFoundException("Project not found");
         }
-        // initial status when status is null;
-        if (projectTask.getPriority() == null || projectTask.getStatus() == "") {
-            projectTask.setStatus("Todo");
-        }
-
-        return projectTaskRepository.save(projectTask);
     }
 
     public Iterable<ProjectTask> findBacklogById(String id) {
+
+        Project project = projectRepository.findByProjectIdentifier(id);
+
+        if (project == null) {
+            throw new ProjectNotFoundException("Project with ID: '" + id + "' does not exist");
+        }
+
         return projectTaskRepository.findByProjectIdentifierOrderByPriority(id);
     }
 }
