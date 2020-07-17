5fcfa0314b308e2fa3b55c31fcb7bdb593463f79
Tom Lam <tomlam0828@gmail.com>
2020-07-16 23:49:15 -0700

    bug fix

--- daily-task-client/src/store.js
+++ daily-task-client/src/store.js
@@ -5,12 +5,15 @@ import rootReducer from "./reducers";
 const initialState = {}
 const middleware = [thunk]
 
-let store
+let store;
 
-if (window.navigator.userAgent.includes("Chrome")) {
+const ReactReduxDevTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+
+if (window.navigator.userAgent.includes("Chrome") && ReactReduxDevTools) {
     store = createStore(rootReducer, initialState, compose(
         applyMiddleware(...middleware),
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+        ReactReduxDevTools
+    )
     )
 } else {
     store = createStore(rootReducer, initialState, compose(
