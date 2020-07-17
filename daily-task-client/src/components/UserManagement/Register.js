9d2d375f873a92d8fe06459204d732e73015a8a7
Tom Lam <tomlam0828@gmail.com>
2020-07-16 17:04:20 -0700

    user error handing

--- daily-task-client/src/components/UserManagement/Register.js
+++ daily-task-client/src/components/UserManagement/Register.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import { connect } from "react-redux";
-import PropTypes from "prop-types";
+import React, { Component } from "react";
 import { createNewUser } from "../../actions/securityActions";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
 import classnames from "classnames";
 
 class Register extends Component {
@@ -10,34 +10,38 @@ class Register extends Component {
 
         this.state = {
             username: "",
+            fullName: "",
             password: "",
             confirmPassword: "",
-            fullName: "",
             errors: {}
-        }
+        };
         this.onChange = this.onChange.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
     }
 
-    onChange(e) {
-        this.setState({
-            [e.target.name]: e.target.value
-        })
+    componentWillReceiveProps(nextProps) {
+        if (nextProps.errors) {
+            this.setState({ errors: nextProps.errors });
+        }
     }
 
     onSubmit(e) {
         e.preventDefault();
         const newUser = {
             username: this.state.username,
-            password: this.state.password,
-            confirmPassword: this.state.confirmPassword,
             fullName: this.state.fullName,
-        }
-
+            password: this.state.password,
+            confirmPassword: this.state.confirmPassword
+        };
         this.props.createNewUser(newUser, this.props.history);
     }
 
+    onChange(e) {
+        this.setState({ [e.target.name]: e.target.value });
+    }
+
     render() {
+        const { errors } = this.state;
         return (
             <div className="register">
                 <div className="container">
@@ -49,43 +53,64 @@ class Register extends Component {
                                 <div className="form-group">
                                     <input
                                         type="text"
-                                        className="form-control form-control-lg"
-                                        placeholder="Name"
+                                        className={classnames("form-control form-control-lg", {
+                                            "is-invalid": errors.fullName
+                                        })}
+                                        placeholder="Full Name"
                                         name="fullName"
                                         value={this.state.fullName}
                                         onChange={this.onChange}
                                     />
+                                    {errors.fullName && (
+                                        <div className="invalid-feedback">{errors.fullName}</div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <input
                                         type="text"
-                                        className="form-control form-control-lg"
+                                        className={classnames("form-control form-control-lg", {
+                                            "is-invalid": errors.username
+                                        })}
                                         placeholder="Email Address (Username)"
                                         name="username"
                                         value={this.state.username}
                                         onChange={this.onChange}
                                     />
-
+                                    {errors.username && (
+                                        <div className="invalid-feedback">{errors.username}</div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <input
                                         type="password"
-                                        className="form-control form-control-lg"
+                                        className={classnames("form-control form-control-lg", {
+                                            "is-invalid": errors.password
+                                        })}
                                         placeholder="Password"
                                         name="password"
                                         value={this.state.password}
                                         onChange={this.onChange}
                                     />
+                                    {errors.password && (
+                                        <div className="invalid-feedback">{errors.password}</div>
+                                    )}
                                 </div>
                                 <div className="form-group">
                                     <input
                                         type="password"
-                                        className="form-control form-control-lg"
+                                        className={classnames("form-control form-control-lg", {
+                                            "is-invalid": errors.confirmPassword
+                                        })}
                                         placeholder="Confirm Password"
                                         name="confirmPassword"
                                         value={this.state.confirmPassword}
                                         onChange={this.onChange}
                                     />
+                                    {errors.confirmPassword && (
+                                        <div className="invalid-feedback">
+                                            {errors.confirmPassword}
+                                        </div>
+                                    )}
                                 </div>
                                 <input type="submit" className="btn btn-info btn-block mt-4" />
                             </form>
@@ -93,17 +118,16 @@ class Register extends Component {
                     </div>
                 </div>
             </div>
-        )
+        );
     }
 }
 
 Register.propTypes = {
     createNewUser: PropTypes.func.isRequired,
     errors: PropTypes.object.isRequired
-}
+};
 
 const mapStateToProps = state => ({
     errors: state.errors
 });
-
 export default connect(mapStateToProps, { createNewUser })(Register);
